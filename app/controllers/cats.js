5017ee62222c3945b6ee925fbc4ce5b8e4358bf8
stschach <stschach@ncsu.edu>
2018-03-25 01:10:26 -0400

    Upload Cats
    -Uploading is a fully functioning feature!
    -Timber, Felix, and Pluto were uploaded to the fray.
    
    -Uploaded cats show up on the leaderboard immediately after upload is complete - no page refresh necessary. They will show up with the highest rank, and with no votes at the bottom of the leaderboard.
    -No protection against the type of image (content and file type can be anything), only the file size.
    -Images are uploaded to the app's firebase storage bucket.
    -A preview of the image about to be uploaded is placed in the preview box.
    -App does not allow an upload to start without a picture.
    -App does not allow unnamed cats to be uploaded.
    
    -emberx-file-input addon installed to most easily display the preview and handle image uploading to firebase.
    -An 'upload' route and controller were created for testing the uploading functionality, and then destroyed. This record is to indicate that testing was done before integrating into the main application.
    -The 'choose file...' button was modified to support x-file-input.
    -The 'select an image of your cat' picture is not clickable yet, css integration of x-file-input is not straightforward.
    
    -[bug] If the bottom rank cat passes the one above it, the rank does not update without a page refresh. This happens while I am able to vote multiple times for any cat on the leaderboard. An error is thrown stating that 'sortedCats' is modified twice. We'll see if it persists after matchup implementation.
    -[FIX] Turns out I was modifying 'sortedCats' twice by calling 'sortedCats' in the 'each' helper and then calling 'computeRank' right underneath it. Excellent. Moving 'computeRank' outside of the 'each' helper removes the issue and maintains functionality.

--- app/controllers/cats.js
+++ app/controllers/cats.js
@@ -29,6 +29,9 @@ export default Controller.extend({
   }),
   sortedCats: computed.sort('model', 'sortBy'),
   sortBy: ['rank'],
+  store: Ember.inject.service(),
+  firebaseApp: Ember.inject.service(),
+  imageIsSelected: false,
 
   actions: {
     addVote(cat) {
@@ -40,6 +43,57 @@ export default Controller.extend({
         cat.decrementProperty('votes');
       }
       cat.save();
+    },
+    didSelectImage(files) {
+      this.set('imageIsSelected', true);
+      let reader = new FileReader();
+      reader.onloadend = Ember.run.bind(this, function() {
+        var dataURL = reader.result;
+        var image = document.getElementById('cat-image');
+        image.src = dataURL;
+        this.set('file', files[0]);
+      });
+      //debugger;
+      reader.readAsDataURL(files[0]);
+    },
+    newCat() {
+      if (!this.get('imageIsSelected')) {
+        alert('Oops..\n\nNo image has been selected! Please click the \'choose file...\' button to select an image to upload.');
+        return;
+      }
+      let catName = this.get('catName');
+      if (!catName) {
+        alert('Please name your cat. You don\'t want us to do it!');
+        return;
+      }
+      this.set('imageIsSelected', false);
+      let store = this.get('store');
+      // Prepare for upload
+      let file = this.get('file');
+      var metadata = {
+        contentType: 'image/png',
+      };
+      const storageRef = this.get('firebaseApp').storage().ref();
+      let path = file.name+'.png';
+      let uploadTask = storageRef.child(path).put(file, metadata);
+      // Begin the upload
+      uploadTask.on('state_changed', function(snapshot) { // progress handler
+        let percent = snapshot.bytesTransferred / snapshot.totalBytes * 100;
+        console.log(percent+'% complete');
+      }, function(error) { // error handler
+        alert('Oh no!\nThe upload failed.\n*Make sure your file is less than 2048x2048 in size.*\nCheck the console or please try again.');
+        console.error('Upload Failed:', error);
+      }, function() { // success handler
+        let uploadUrl = uploadTask.snapshot.metadata.downloadURLs[0];
+        console.log('File available at ', uploadUrl);
+        // Create a new cat model record
+        let cat = store.createRecord('cat', { // ERROR: this.get('store') is not a function :(
+          name: catName,
+          imageUrl: uploadUrl
+        });
+        cat.save();
+      });
+      this.set('catName', '');
     }
   }
 });
--- app/models/cat.js
+++ app/models/cat.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   imageUrl: DS.attr('string'),
   name: DS.attr('string'),
-  rank: DS.attr('number', {defaultValue: 0}),
+  rank: DS.attr('number', {defaultValue: 99}),
   votes: DS.attr('number', {defaultValue: 0})
 });
