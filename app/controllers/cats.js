f81de911ef2501c894db7b76e3c7a7430b762880
stschach <stschach@ncsu.edu>
2018-03-31 01:20:27 -0400

    tiebreaker + cleanUp_1
    -Preview image can be clicked on to upload an image.
    -Code cleaned up a bit for aesthetic improvement.
    -Installed add-on ember-composable-helpers to use a pipeline of actions associated with clicking an image to vote on a cat. Ultimately, this is in place to force the process of determining matchup cats to run only once as an action, instead of running erratically as a template-rendered computed property. Works as expected.
    -Added a 'Go!' button for more fun, and forcing the action handler 'determineSelected' to be called on the initially-loaded empty images.
    -New file path for uploads forces a unique filename for each upload, even if the filenames are the same.
    -Minor style additions.
    
    -Empty images being displayed due to ties in the ranks has been resolved. Tiebreakers are presented if one of the cats is tied in rank with another.
    --An infinite loop condition has been introduced (see line 124 of controllers/cats.js) in order to find an existing cat rank. It is entirely dependent on Math.random() eventually returning a number that matches a rank of one of the cats. The longest this might possibly run is in a situation where there are n cats all at the same rank. Eventually, in practice, the function returns to end the loop.

--- app/controllers/cats.js
+++ app/controllers/cats.js
@@ -1,116 +1,151 @@
 import Controller from '@ember/controller';
-import { mapBy, alias } from '@ember/object/computed';
-import { set } from '@ember/object';
-import { computed } from '@ember/object';
-import DS from 'ember-data';
+import {
+  set,
+  computed
+} from '@ember/object';
+import {
+  mapBy,
+  alias
+} from '@ember/object/computed';
 
 export default Controller.extend({
+  battleHasBegun: false,
   listOfVotes: mapBy('model', 'votes'),
   listOfRanks: mapBy('model', 'rank'),
   totalCatz: alias('model.length'),
+  sortedCats: computed.sort('model', 'sortedBy'),
+  sortedBy: ['rank'],
   leftCatId: null,
   rightCatId: null,
-  determineSelected: computed('model.@each.votes', function() {
-    let catz = this.get('model');
-    let rankList = this.get('listOfRanks');
-    let leftRankExists = false;
-    let leftCatSet = false;
-    let rightRankExists = false;
-    let rightCatSet = false;
-    let totalCats = this.get('totalCatz');
-    var leftCat = document.getElementById('contestant-left');
-    var rightCat = document.getElementById('contestant-right');
-    let catDisplayLeft = Math.floor((Math.random() * totalCats) + 1);
-    let catDisplayRight = Math.floor((Math.random() * totalCats) + 1);
-    // Eliminate displaying same cat on both sides
-    while (catDisplayLeft == catDisplayRight) {
-      catDisplayRight = Math.floor((Math.random() * totalCats) + 1);
-    }
-    // Eliminate displaying no cat in case there is a tie
-    // function findExistingRank(catRankLeft, catRankRight) {
-    //   for (let i = 0; i < totalCats; i++) {
-    //     if (catRankLeft == rankList[i]) {
-    //       leftRankExists = true;
-    //       return catRankLeft;
-    //     }
-    //     if (catRankRight == rankList[i]) {
-    //       rightRankExists = true;
-    //       return catRankRight;
-    //     }
-    //   }
-    //   catDisplayLeft = rankList[2];
-    //   catDisplayRight = rankList[3];
-    //   return;
-    // }
-    // if (!leftRankExists) { catDisplayLeft = findExistingRank(catDisplayLeft+1); }
-    // console.log('left rank = '+catDisplayLeft);
-    // console.log('right rank = '+catDisplayRight);
-    // Display cats according to their randomly chosen rank
-    catz.forEach((cat) => {
-      if (cat.get('rank') == catDisplayLeft) { // pick cat to show on the right
-        leftCat.src = cat.get('imageUrl');
-        this.set('leftCatId', cat);
-        leftCatSet = true;
-        // console.log(this.get('leftCatId.name'));
-      }
-      if (cat.get('rank') == catDisplayRight) { // pick cat to show on the left
-        rightCat.src = cat.get('imageUrl');
-        this.set('rightCatId', cat);
-        rightCatSet = true;
-        // console.log(this.get('rightCatId.name'));
-      }
-    })
-    if (!leftCatSet || !rightCatSet) {
-      alert('Whoops.. something went wrong! Please reload the page.');
-    }
-    return;
-  }),
   computeRank: computed('model.@each.votes', function() {
     let catz = this.get('model');
     let voteList = this.get('listOfVotes');
     let numberOfCuterCats = 0;
     catz.forEach((cat) => {
-      for (let i=0; i<this.get('model.length'); i++) {
+      for (let i = 0; i < this.get('model.length'); i++) {
         if (cat.get('votes') < voteList[i]) {
           numberOfCuterCats++;
         }
       }
       if (numberOfCuterCats == 0) {
         set(cat, 'rank', 1);
-      }
-      else {
-        set(cat, 'rank', numberOfCuterCats+1);
+      } else {
+        set(cat, 'rank', numberOfCuterCats + 1);
       }
       numberOfCuterCats = 0;
     })
     catz.save();
     return;
   }),
-  sortedCats: computed.sort('model', 'sortBy'),
-  sortBy: ['rank'],
   store: Ember.inject.service(),
   firebaseApp: Ember.inject.service(),
   imageIsSelected: false,
 
   actions: {
+    startBattle() {
+      this.set('battleHasBegun', true);
+    },
     addVoteLeft() {
       let cat = this.get('leftCatId');
-      // console.log('vote added to '+cat.name);
       cat.incrementProperty('votes');
       cat.save();
     },
     addVoteRight() {
       let cat = this.get('rightCatId');
-      // console.log('vote added to '+cat.name);
       cat.incrementProperty('votes');
       cat.save();
     },
     // Admin handler
     removeVote(cat) {
-      if(cat.get('votes') > 0) {
+      if (cat.get('votes') > 0) {
         cat.decrementProperty('votes');
       }
       cat.save();
+    },
+    determineSelected() {
+      let catz = this.get('model');
+      let rankList = this.get('listOfRanks');
+      let leftRankExists = false;
+      let leftCatSet = false;
+      let rightRankExists = false;
+      let rightCatSet = false;
+      let totalCats = this.get('totalCatz');
+      var leftCat = document.getElementById('contestant-left');
+      var rightCat = document.getElementById('contestant-right');
+      let catDisplayLeft = Math.floor((Math.random() * totalCats) + 1);
+      let catDisplayRight = Math.floor((Math.random() * totalCats) + 1);
+
+      // Eliminate displaying same cat on both sides
+      while (catDisplayLeft == catDisplayRight) {
+        catDisplayRight = Math.floor((Math.random() * totalCats) + 1);
+      }
+
+      // If there is a tie with one of the ranks selected, present a tiebreaker
+      function tiebreaker(catRank) {
+        let sameCat = true;
+        let numberOfTiedCats = 0;
+        for (let i = 0; i < totalCats; i++) {
+          if ((catRank == rankList[i]) && !sameCat) {
+            numberOfTiedCats++;
+          } else if (catRank == rankList[i]) {
+            sameCat = false;
+          }
+        }
+        if (numberOfTiedCats > 0) {
+          catDisplayLeft = catRank;
+          catDisplayRight = catRank;
+        }
+        return;
+      }
+      tiebreaker(catDisplayLeft);
+      tiebreaker(catDisplayRight);
+
+      // Eliminate displaying no cat in case there is a tie
+      function findExistingLeftRank(catRankLeft) {
+        for (let i = 0; i < totalCats; i++) {
+          if (catRankLeft == rankList[i]) {
+            leftRankExists = true;
+            return catRankLeft;
+          }
+        }
+        while (!leftRankExists) {
+          catDisplayLeft = Math.floor((Math.random() * totalCats) + 1);
+          findExistingLeftRank(catDisplayLeft);
+        }
+      }
+
+      function findExistingRightRank(catRankRight) {
+        for (let i = 0; i < totalCats; i++) {
+          if (catRankRight == rankList[i]) {
+            rightRankExists = true;
+            return catRankRight;
+          }
+        }
+        while (!rightRankExists) {
+          catDisplayRight = Math.floor((Math.random() * totalCats) + 1);
+          findExistingRightRank(catDisplayRight);
+        }
+      }
+
+      // Display cats according to the chosen ranks
+      catz.forEach((cat) => {
+        if (cat.get('rank') == catDisplayLeft) { // find cat to show on the left
+          leftCat.src = cat.get('imageUrl');
+          this.set('leftCatId', cat);
+          leftCatSet = true;
+          catDisplayLeft = null;
+        } else if (cat.get('rank') == catDisplayRight) { // find cat to show on the right
+          rightCat.src = cat.get('imageUrl');
+          this.set('rightCatId', cat);
+          rightCatSet = true;
+          catDisplayRight = null;
+        }
+      })
+      if (!leftCatSet) {
+        findExistingLeftRank(catDisplayLeft);
+      } else if (!rightCatSet) {
+        findExistingRightRank(catDisplayRight);
+      }
     },
     didSelectImage(files) {
       this.set('imageIsSelected', true);
@@ -136,26 +171,29 @@ export default Controller.extend({
       }
       this.set('imageIsSelected', false);
       let store = this.get('store');
+
       // Prepare for upload
       let file = this.get('file');
       var metadata = {
         contentType: 'image/png',
       };
       const storageRef = this.get('firebaseApp').storage().ref();
-      let path = file.name+'.png';
+      let path = file.name + this.get('totalCatz') + '.png';
       let uploadTask = storageRef.child(path).put(file, metadata);
       var uploadButton = document.getElementById('upload-button').firstChild;
+
       // Begin the upload
       uploadTask.on('state_changed', function(snapshot) { // progress handler
         let percent = snapshot.bytesTransferred / snapshot.totalBytes * 100;
-        uploadButton.data = Math.round(percent)+'%';
-        console.log(percent+'% complete');
+        uploadButton.data = Math.round(percent) + '%';
+        console.log(percent + '% complete');
       }, function(error) { // error handler
         alert('Oh no!\nThe upload failed.\n*Make sure your file is less than 2048x2048 in size.*\nCheck the console or please try again.');
         console.error('Upload Failed:', error);
       }, function() { // success handler
         let uploadUrl = uploadTask.snapshot.metadata.downloadURLs[0];
         console.log('File available at ', uploadUrl);
+
         // Create a new cat model record
         let cat = store.createRecord('cat', {
           name: catName,
@@ -163,7 +201,9 @@ export default Controller.extend({
         });
         cat.save();
         uploadButton.data = 'Success!'; // indicates upload is complete
-        setTimeout(function(){uploadButton.data = 'Upload!';}, 3000); // restores button text after 3 seconds
+        setTimeout(function() {
+          uploadButton.data = 'Upload!';
+        }, 3000); // restores button text after 3 seconds
       });
       this.set('catName', '');
     }
